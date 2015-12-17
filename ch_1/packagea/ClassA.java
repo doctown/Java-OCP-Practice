bb90f1bad3e4d02f9db0217bbe58de93812fcfcc
David Ogor <doctown@yahoo.com>
2015-12-17 08:17:08 -0500

    Chapter 1 practic

--- ch_1/packagea/ClassA.java
+++ ch_1/packagea/ClassA.java
@@ -7,4 +7,13 @@ public class ClassA {
 	public static void main(String args[]) {
 		System.out.println("Class A");
 	}
+	
+	void temp() {
+		
+	}
+	
+	protected void finalize() {
+		System.out.println("Class A is being garbage colleted.");
+		
+	}
 }
--- ch_1/packageb/ClassB.java
+++ ch_1/packageb/ClassB.java
@@ -3,10 +3,31 @@ import packagea.*;
 
 public class ClassB {
 	public static void main(String ... args) {
-			int b, c, d = c = b = 0;
-			System.out.println("" + b + c + d);
-			System.out.println("ClassB");
-			ClassA a = new ClassA();
-			ClassA.main(new String[] {"e","\""});
-		}
+			
+			
+	}
+	
+	
+	public static void testClass() {
+		int b, c, d = c = b = 0;
+		System.out.println("" + b + c + d);
+		System.out.println("ClassB");
+		ClassA a = new ClassA();
+		ClassA.main(new String[] {"e","\""});
+		a = null;
+
+		int x = 5;
+		int y = x = 3;
+		
+		if (3.1 <= y)
+			System.out.println(y);
+	}
+
+	public static void garbageCollectionTest() {
+			//System.gc();
+			//Runtime rc = new Runtime();
+			//rc.gc();
+			System.out.println("Ending ClassB.");	
+			
+	}
 }
