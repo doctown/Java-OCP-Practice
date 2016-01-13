ef3591ce2c8142bc6878a5f3b9956e1464da22e4
David Ogor <doctown@yahoo.com>
2016-01-13 18:13:32 -0500

    View locales on computer

--- ch8_strings_locale/ResourceBundlePractice.java
+++ ch8_strings_locale/ResourceBundlePractice.java
@@ -7,20 +7,39 @@
  * Chapter 8:  String Processing, Data Formatting, Resource Bundles
  * Date: 08-19-2015
  */
- package ch8;
+package ch8_strings_locale;
  
- import java.util.Locale;
- import java.util.ResourceBundle;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Arrays;
+import java.util.*;
  
- public class ResourceBundlePractice {
+class AvailableLocales {
+	public void showLocales() {
+		System.out.println("The default locale is: " + Locale.getDefault());
+		Locale[] locales = Locale.getAvailableLocales();
+		System.out.printf("There are %d locales\n", locales.length);
+		Arrays.sort(locales, new Comparator<Locale>() {
+			public int compare(Locale one, Locale another) {
+				return one.toString().compareTo(another.toString());
+			}
+		});
+		
+		for (Locale loc : locales) {
+			System.out.printf("%15s%s which stands for %s %n","Locale code: ", loc, loc.getDisplayName());
+		}
+	}
+}
+ 
+public class ResourceBundlePractice {
 	public static void main(String[] args) {
 		if (args.length == 0) {
-			System.out.println("Usage error: ResourceBundlePractice [locale] ");
+			new AvailableLocales().showLocales();
 		} else {
 			resourceBundleTest(args);
 		}
 	}
-	
+
 	/** 
 	 * Create a resource bundle.
 	 */
@@ -33,4 +52,4 @@
 		
 		System.out.println(rb.getString("hello"));
 	}
- }
+}
